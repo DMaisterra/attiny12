65b105765a8fa17b87a3021b1f9acd3ed1e966ad
marcolinuxbr <marcolinuxbr@yahoo.com.br>
2014-02-17 00:02:00 -0300

    Seg Fev 17 00:02:00 BRT 2014

--- tiny12.sh
+++ tiny12.sh
@@ -2,10 +2,10 @@
 
 PROGNAME=$1
 
-rm -f *.o *.elf *.out *.?hex
+rm -f *.o *.elf *.out *.ihex *.hex 
 
 avr-gcc -c -mmcu=attiny12 -o $PROGNAME.o $PROGNAME.S &&\
 avr-ld -o $PROGNAME.elf $PROGNAME.o &&\
 avr-objcopy --output-target=ihex $PROGNAME.elf $PROGNAME.ihex &&\
 avrdude -c usbasp -p t12 -U flash:w:$PROGNAME.ihex   &&\
-rm -f *.o *.elf *.out *.?hex
+rm -f *.o *.elf *.out *.?hex *.hex
