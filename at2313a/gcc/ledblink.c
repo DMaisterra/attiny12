a92c8bde6995f34cfd90c0e6004516ded7d36606
Mark Linux <marcolinuxbr@yahoo.com.br>
2014-02-21 17:57:26 -0300

    20140221-1757

--- at2313a/gcc/ledblink.c
+++ at2313a/gcc/ledblink.c
@@ -12,17 +12,32 @@
 #define outPin2 PD3
 
 
+typedef struct {
+ unsigned char b0:1;
+ unsigned char b1:1;
+ unsigned char b2:1;
+ unsigned char b3:1;
+ unsigned char b4:1;
+ unsigned char b5:1;
+ unsigned char b6:1;
+ unsigned char b7:1;
+} bits_t;
 
-void onLed(uint8_t ledNum){
-    switch(ledNum++){
-    case 0: {
-	outPort &= ~(1<<outPin0);break;
-    }
-    
+
+
+void ledOn(uint8_t ledNum){
+    switch(ledNum){
+    case 0: 
+	{outPort &= ~(1<<outPin0);}break;
     }
 }
 
-
+void ledOff(uint8_t ledNum){
+    switch(ledNum){
+    case 0: 
+	{outPort |= (1<<outPin0);}break;
+    }
+}
 
 
 void sleep(uint8_t millisec){
@@ -33,23 +48,29 @@ void sleep(uint8_t millisec){
 }
 
 
-
-
 main(){
+    //IO output
     dirPort |=(1<<outPin0);
     dirPort |=(1<<outPin1);
+    //all IO = 0
     outPort &= ~(1<<outPin0);
     outPort &= ~(1<<outPin1);
 
+
+bits_t bit_array[10];
+
+bit_array[1].b3 = 1;
+
+
     while(1){
-        outPort &= ~(1<<outPin0);
+        ledOn(0);
         sleep(255);
         sleep(255);
-        outPort |=(1<<outPin0);
+        ledOff(0);
         sleep(255);
         sleep(255);
-        sleep(255);
-        sleep(255);
+//        sleep(255);
+//        sleep(255);
         }
 }
 
--- at2313a/gcc/ledblink.sh
+++ at2313a/gcc/ledblink.sh
@@ -1,10 +1,4 @@
 
-# https://electronics.stackexchange.com/questions/2377/attiny13-avr-gcc-hello-world-uses-over-100-bytes
-# www.tldp.org/HOWTO/Avr-Microcontrollers-in-Linux-Howto/x207.html
-
-#avr-gcc -mmcu=atmega8 -Os ledblink.c -o ledblink.o -S
-#avr-objcopy -j .text -j .data -O ihex  ledblink.o  ledblink.hex
-
 rm -f *.o *.elf *.out *.?hex *.hex *.S
 
 avr-gcc -mmcu=attiny2313 -Os  ledblink.c -o ledblink.o &&\
