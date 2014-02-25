59a4f8f5813134bbd3d0a1cae15184ebcec1eacf
Mark Linux <marcolinuxbr@yahoo.com.br>
2014-02-25 14:17:22 -0300

    eclipse

--- at2313a/gcc/ledblink.c
+++ at2313a/gcc/ledblink.c
@@ -4,25 +4,36 @@
 
 #include <avr/io.h>
 #include <util/delay.h>
+#include <avr/interrupt.h> 
 
-#define outPort PORTD
 #define dirPort DDRD
 #define outPin0 PD5
 #define outPin1 PD4
 #define outPin2 PD3
-
+#define outPort (*((volatile bits_t*)(&PORTD)))
 
 typedef struct {
- unsigned char b0:1;
- unsigned char b1:1;
- unsigned char b2:1;
- unsigned char b3:1;
- unsigned char b4:1;
- unsigned char b5:1;
- unsigned char b6:1;
- unsigned char b7:1;
+ unsigned char bit0:1;
+ unsigned char bit1:1;
+ unsigned char bit2:1;
+ unsigned char bit3:1;
+ unsigned char bit4:1;
+ unsigned char bit5:1;
+ unsigned char bit6:1;
+ unsigned char bit7:1;
 } bits_t;
 
+
+
+//teste gcc
+#define set_bits_macro(port,mask) ((port) |= (mask))
+
+
+char err_str[] = "Your program has lies !";
+
+//teste gcc - fim
+
+volatile bits_t bit_array[1];
 
 
 void ledOn(uint8_t ledNum){
@@ -47,8 +58,18 @@ void sleep(uint8_t millisec){
     }
 }
 
+void timer0(void)
+{
+   TCCR0A = 0x00;                //Timer counter control register
+   TCCR0B = (0 << WGM02)|(1 << CS02)|(0 << CS01)|(0 << CS00); // WGM=0, prescale at 8
+         TIMSK |= (1 << TOIE0);         //Set bit 1 in TIMSK to enable Timer 1 overflow interrupt.
+    }
 
 main(){
+
+    timer0();
+    sei();
+
     //IO output
     dirPort |=(1<<outPin0);
     dirPort |=(1<<outPin1);
@@ -56,21 +77,30 @@ main(){
     outPort &= ~(1<<outPin0);
     outPort &= ~(1<<outPin1);
 
+    set_bits_macro (outPort, 0xf0);
 
-bits_t bit_array[10];
 
-bit_array[1].b3 = 1;
+bit_array[0].b0 = 1;
+
+
 
 
     while(1){
         ledOn(0);
+        //sleep(55);
         sleep(255);
+        //ledOff(0);
         sleep(255);
-        ledOff(0);
-        sleep(255);
-        sleep(255);
-//        sleep(255);
+        //sleep(255);
+        //sleep(255);
 //        sleep(255);
         }
 }
 
+ISR (TIMER0_OVF_vect)
+{
+
+     outPort.bit5 = bit_array[0].b0 ;
+
+   //ledOff(0);
+}
